64451586b587dd13897456fa3ea16b393ee79990
Yohsuke Fukai <ysk@yfukai.net>
2022-02-06 12:59:02 +0900

    moved files

--- nextflow/main.nf
+++ nextflow/main.nf
@@ -3,22 +3,23 @@ params.output_path = "."
 
 Channel.fromPath("${params.input_path}/**.czi").view({ "${it}" }).set({ input_czi_files })
 
-profiles {
-    conda {
-//        process.conda = "${moduleDir}/envs/image_analysis.yaml"
-    }
-}
+//profiles {
+//    conda {
+////        process.conda = "${moduleDir}/envs/image_analysis.yaml"
+//        process.conda = "czi_shadow_correction_stitching"
+//    }
+//}
 
 process exportMetadata {
     input : 
     file czi_file from input_czi_files
 
     output :
-    tuple file(czi_file), file("metadata_yaml.yaml") into fileMetadata
+    tuple file(czi_file), file("metadata.yaml") into fileMetadata
 
     """
     ${moduleDir}/scripts/a_export_metadata.py \
-        --input_czi ${czi_file} \
-        --output_metadata_yaml metadata_yaml.yaml
+        ${czi_file} \
+        metadata.yaml
     """
 }
--- nextflow/scripts/a_export_metadata.py
+++ nextflow/scripts/a_export_metadata.py
@@ -7,11 +7,21 @@ import yaml
 @click.argument('input_czi', type=click.Path(exists=True))
 @click.argument("output_metadata_yaml", type=click.Path())
 def main(input_czi, output_metadata_yaml):
-    aics_image = AICSImage(input_czi)
+    aics_image = AICSImage(input_czi,reconstruct_mosaic=False)
+    dims=aics_image.dims
     metadata=dict(
-        channels = aics_image.channel_names,
-        dims = aics_image.dims,
-        physical_pixel_sizes = aics_image.physical_pixel_sizes
+        channel_names = list(map(str,aics_image.channel_names)), # channel name strings
+        dims = dict(dims.items()), # dimensions of the image
+        mosaic_positions = [list(aics_image.get_mosaic_tile_position(i)) 
+                            for i in range(dims.M)], # Y and X mosaic positions in pixel
+        physical_pixel_sizes = [
+            aics_image.physical_pixel_sizes.Z,
+            aics_image.physical_pixel_sizes.Y,
+            aics_image.physical_pixel_sizes.X
+        ] # physical pixel sizes in microns, in order of Z, Y, X
     )
     with open(output_metadata_yaml, "w") as f:
         yaml.dump(metadata, f)
+
+if __name__ == '__main__':
+    main()
